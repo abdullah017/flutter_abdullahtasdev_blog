a36a967f5b3ff932635f5ea5f33e67992cf8c37a
abdullah017 <abdullahozvanli@gmail.com>
2024-12-22 15:34:07 +0300

    [Refactor] Post Add Controller and Binding

--- lib/presentation/admin/bindings/post_add_binding.dart
+++ lib/presentation/admin/bindings/post_add_binding.dart
@@ -1,3 +1,4 @@
+import 'package:abdullahtasdev/core/api/firebase/storage_service.dart';
 import 'package:abdullahtasdev/data/repositories/admin_repositories/post_repositories.dart';
 import 'package:abdullahtasdev/presentation/admin/controllers/post_add_controller.dart';
 import 'package:get/get.dart';
@@ -5,7 +6,8 @@ import 'package:get/get.dart';
 class PostAddBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<PostAddController>(
-        () => PostAddController(postRepository: Get.put(PostRepository())));
+    Get.lazyPut<PostAddController>(() => PostAddController(
+        postRepository: Get.put(PostRepository()),
+        storageService: Get.put(StorageService())));
   }
 }
